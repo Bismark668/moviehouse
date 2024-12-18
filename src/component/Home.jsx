d9e33062a61cd3a55e490472b70f0b3570ceed0b
Bismark668 <147917491+Bismark668@users.noreply.github.com>
2024-12-17 21:11:31 -0600

    bugs fixed

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,59 +1,91 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from "react";
 import { IoMdSearch } from "react-icons/io";
-import { useEffect } from 'react';
-import logo from '../assets/logo.png'
-import { MovieCard } from './MovieCard';
-import './Home.css'
+import logo from "../assets/logo.png";
+import { MovieCard } from "./MovieCard";
+import "./Home.css";
 
-const API_URL = 'http://www.omdbapi.com/?apikey=c032e2d7&'
+const API_URL = "https://www.omdbapi.com/?apikey=c032e2d7";
 
-
+const debounce = (func, delay) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
 
 export const Home = () => {
-  const [movies,setMovies] = useState([])
-  const [searchMovies, setsearchMovies] = useState('')
+  const [movies, setMovies] = useState([]);
+  const [searchMovies, setsearchMovies] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
+  const searchNewMovie = async (title) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+      const data = await response.json();
 
-  const searchNewMovie = async(title) => {
-    const response = await fetch(`${API_URL}&S=${title}`)
-    const data = await response.json()
+      if (data.Response === "True") {
+        setMovies(data.Search);
+      } else {
+        setMovies([]);
+      }
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+      setMovies([]);
+    }
+    setIsLoading(false);
+  };
 
-    setMovies(data.Search)
-  }
+  const searchNewMovieDebounced = useCallback(
+    debounce((title) => searchNewMovie(title), 500),
+    []
+  );
 
   useEffect(() => {
-    searchNewMovie('Rambo')
-  },[])
+    searchNewMovie("Rambo");
+  }, []);
 
-  const movie1={
-    "Title": "The Lord of the Rings: The Battle for Middle-earth II - The Rise of the Witch-king",
-    "Year": "2006",
-    "imdbID": "tt1058040",
-    "Type": "game",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMjYwMDIxNjg3MV5BMl5BanBnXkFtZTgwMTk5MTE4MDE@._V1_SX300.jpg"
-}
   return (
     <div className="home-div">
       <div className="nav-bar">
-        <div className="logo"><img src={logo} alt="logo" /></div>
-        <div className="serac-bar"><input  type="text" value={searchMovies} id='search' placeholder='Search All Movies' onChange={(e) =>setsearchMovies(e.target.value)}/>
-        <IoMdSearch className='icon' onClick={()=> searchNewMovie(searchMovies)}/></div>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="search-bar">
+          <input
+            type="text"
+            value={searchMovies}
+            id="search"
+            placeholder="Search All Movies"
+            onChange={(e) => {
+              setsearchMovies(e.target.value);
+              searchNewMovieDebounced(e.target.value);
+            }}
+          />
+          <IoMdSearch
+            className="icon"
+            onClick={() => searchNewMovie(searchMovies)}
+          />
+        </div>
       </div>
 
-      {
-        movies?.length > 0
-          ?(
-            <div className="content">
-              { movies.map((movie) => (
-                <MovieCard movie={movie} />
-              ))}
-            </div>
-          ) : (
-              <div className="nomovies">
-                <h2>No Movies Found</h2>
-              </div>
-          )
-      }
+      {isLoading ? (
+        <div className="loading">Loading...</div>
+      ) : movies && movies.length > 0 ? (
+        <div className="content">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.imdbID} />
+          ))}
+        </div>
+      ) : (
+        <div className="nomovies">
+          <h2>No Movies Found</h2>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
